632c833c04a2ecdf95c6f61a04a3417774cf1014
Ana Teresa Cruz <up201806460@fe.up.pt>
2020-10-07 14:13:11 +0100

    Added cylinder side and bases

--- TP1/primitives/MyCylinder.js
+++ TP1/primitives/MyCylinder.js
@@ -4,63 +4,35 @@
  */
 class MyCylinder extends CGFobject {
 	constructor(scene, height, topRadius, bottomRadius, stacks, slices) {
-		super(scene);
-		this.height = height;
-		this.topRadius = topRadius;
-		this.bottomRadius = bottomRadius;
-		this.stacks = stacks;
-		this.slices = slices;
-		this.initBuffers();
+        super(scene);
+        this.height = height;
+        this.topRadius = topRadius;
+        this.bottomRadius = bottomRadius;
+        this.stacks = stacks;
+        this.slices = slices;
+
+        this.cylinder_side = new MyCylinderSide(scene, height, topRadius, bottomRadius, stacks, slices);
+        this.top_circle = new MyCircle(scene, topRadius, slices);
+        this.bottom_circle = new MyCircle(scene, bottomRadius, slices);
 		
 	}
 	
-	initBuffers() {
-        this.vertices = [];
-        this.indices = [];
-        this.normals = [];
-    
-		var angle = 0;
-		var beta = 2*Math.PI/this.slices;
+	
+    display(){
+        //Top
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, this.height);
+        this.top_circle.display();
+        this.scene.popMatrix();
 
-		for(var i = 0; i <= this.stacks; i++){
-			var radius = (this.topRadius - this.bottomRadius)*(i/this.stacks) + this.bottomRadius;
-			var h = this.height * i / this.stacks;
-			for(var j = 0; j <= this.slices; j++){
-				var x_ang = Math.cos(angle) * radius;
-				var z_ang = Math.sin(angle) * radius;
-				this.vertices.push(x_ang, z_ang, h);
-				this.normals.push(x_ang, z_ang, 0);
-				angle += beta;
-			}
+        //Bottom
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI, 1, 0, 0);
+        this.bottom_circle.display();
+        this.scene.popMatrix();
 
-		}
-
-		for(var i = 0; i < this.stacks; i++){
-			for(var j = 0; j < this.slices; j++){
-				var ind1 = j + i * (this.slices+1);
-				var ind2 = j + i *(this.slices+1)+1;
-				var ind3 = j + (i+1)*(this.slices+1);
-				var ind4 = j + (i+1)*(this.slices+1)+1;
-				this.indices.push(ind4, ind3, ind1);
-				this.indices.push(ind1,ind2,ind4);
-
-			}
-
-		}
-
-
-
-		this.primitiveType = this.scene.gl.TRIANGLES;
-		this.initGLBuffers();
-	}
-
-
-	updateBuffers(complexity){
-        this.slices = 3 + Math.round(9 * complexity); //complexity varies 0-1, so slices varies 3-12
-
-        // reinitialize buffers
-        this.initBuffers();
-        this.initNormalVizBuffers();
+        this.cylinder_side.display();
     }
+    
 }
 
