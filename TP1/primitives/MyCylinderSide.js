ba4f395d93ce6381517ef3593cce9bbb8503132a
Ana Teresa Cruz <up201806460@fe.up.pt>
2020-10-07 14:12:11 +0100

    Added side of the cylinder

---
+++ TP1/primitives/MyCylinderSide.js
@@ -0,0 +1,68 @@
+/**
+ * MyCylinderSide
+ * @constructor
+ */
+class MyCylinderSide extends CGFobject {
+	constructor(scene, height, topRadius, bottomRadius, stacks, slices) {
+		super(scene);
+		this.height = height;
+		this.topRadius = topRadius;
+		this.bottomRadius = bottomRadius;
+		this.stacks = stacks;
+		this.slices = slices;
+
+		this.initBuffers();
+		
+	}
+	
+	initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+		this.normals = [];
+		this.texCoords = [];
+    
+		var angle = 0;
+		var beta = 2*Math.PI/this.slices;
+
+		for(var i = 0; i <= this.stacks; i++){
+			var radius = (this.topRadius - this.bottomRadius)*(i/this.stacks) + this.bottomRadius;
+			var h = this.height * i / this.stacks;
+			for(var j = 0; j <= this.slices; j++){
+				var x_ang = Math.cos(angle) * radius;
+				var z_ang = Math.sin(angle) * radius;
+				this.vertices.push(x_ang, z_ang, h);
+				this.normals.push(x_ang, z_ang, 0);
+				this.texCoords.push(j*1/this.slices, 1- (i*1/this.stacks));
+				angle += beta;
+			}
+
+		}
+
+		for(var i = 0; i < this.stacks; i++){
+			for(var j = 0; j < this.slices; j++){
+				var ind1 = j + i * (this.slices+1);
+				var ind2 = j + i *(this.slices+1)+1;
+				var ind3 = j + (i+1)*(this.slices+1);
+				var ind4 = j + (i+1)*(this.slices+1)+1;
+				this.indices.push(ind4, ind3, ind1);
+				this.indices.push(ind1,ind2,ind4);
+
+			}
+
+		}
+
+
+
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	}
+
+	updateBuffers(complexity){
+        this.slices = 3 + Math.round(9 * complexity); //complexity varies 0-1, so slices varies 3-12
+
+        // reinitialize buffers
+        this.initBuffers();
+        this.initNormalVizBuffers();
+    }
+}
+
