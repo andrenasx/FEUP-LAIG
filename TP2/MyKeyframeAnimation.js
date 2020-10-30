8f004be54cfdc4403d4f85c1a2255ae15a4f53ef
Ana Teresa Cruz <up201806460@fe.up.pt>
2020-10-30 15:22:51 +0000

    Continuing the keyframeAnimation class, added functions to aplly and update

--- TP2/MyKeyframeAnimation.js
+++ TP2/MyKeyframeAnimation.js
@@ -1,6 +1,36 @@
 class MyKeyframeAnimation extends MyAnimation{
     constructor(keyframes){
-        super(keyframes[0].instant, keyframes[keyframes.length-1].instant, keyframes[0].transformation, keyframes[keyframes.length-1].transformation);
+        this.start = this.keyframes[0].instant;
+        this.frame = 1;
+        this.elapsedTime = 0;
+        this.isFinished = false;
         this.keyframes = keyframes;
+        if(this.keyframes.length === 1)
+            this.activeAnimation = new MyAnimation(this.keyframes[this.frame-1].instant, this.keyframes[this.frame-1].instant, this.keyframes[this.frame-1].transformations, this.keyframes[this.frame-1]);
+        else
+            this.activeAnimation = new MyAnimation(this.keyframes[this.frame-1].instant, this.keyframes[this.frame].instant, this.keyframes[this.frame-1].transformations, this.keyframes[this.frame]);
+    }
+
+    apply(scene){
+        this.activeAnimation.apply(scene);
+    }
+
+    update(delta){
+        if(this.frame === this.keyframes.length)
+            return;
+
+        this.activeAnimation.update(delta);
+
+        if(this.elapsedTime > this.keyframes[this.frame].instant){
+            if(this.activeAnimation.isFinished){
+                this.frame++;
+                if(this.frame === this.keyframes.length)
+                    return;
+                
+                this.activeAnimation = new MyAnimation(0, this.keyframes[this.frame].instant-this.keyframes[this.frame-1].instant, this.keyframes[this.frame-1].transformations, this.keyframes[this.frame]);
+            }
+        }
+        this.elapsedTime += delta;
     }
 }
+
