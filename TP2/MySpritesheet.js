04a40510450dd3406eda7c3fc5fc378da6a2babb
Ana Teresa Cruz <up201806460@fe.up.pt>
2020-11-04 11:13:10 +0000

    Added class spritesheet

---
+++ TP2/MySpritesheet.js
@@ -0,0 +1,22 @@
+class MySpritesheet extends CGFobject {
+	constructor(scene, texture, sizeM, sizeN) {
+		super(scene);
+        this.texture = texture;
+        this.sizeM = sizeM;
+        this.sizeN = sizeN;
+
+        this.shader = new CGFshader(this.scene.gl, "spritesheets/shader.vert", "spritesheets/shader.frag");
+        this.shader.setUniformsValues({cols:this.sizeM});
+        this.shader.setUniformsValues({rows:this.sizeN});
+        this.shader.setUniformsValues({uSampler:0});
+    }
+
+    activateCellMN(m, n){
+        this.shader.setUniformsValues({m:m});
+        this.shader.setUniformsValues({n:n});
+    }
+
+    activateCellP(p){
+        this.activateCellMN(p % this.sizeM, p / this.sizeN);
+    }
+}
