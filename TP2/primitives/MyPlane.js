e15de9ed6ca35da6a3deab444a0319de5426414e
Ana Teresa Cruz <up201806460@fe.up.pt>
2020-11-16 16:20:40 +0000

    Added MyPlane class

---
+++ TP2/primitives/MyPlane.js
@@ -0,0 +1,34 @@
+/**
+ * MyPlane
+ * @constructor
+ * @param {CGFscene} scene - Reference to MyScene object
+ * @param {integer} npartsU - number of divisions in the U direction
+ * @param {integer} npartsV - number of divisions in the V direction
+ */
+class MyPlane extends CGFobject {
+	constructor(scene, npartsU, npartsV) {
+        super(scene);
+        this.npartsU = npartsU;
+        this.npartsV = npartsV;
+
+        const controlPoints = 
+		[
+			[
+				[-0.5, 0.0,  0.5],	
+				[-0.5, 0.0, -0.5]	
+			],
+			
+			[
+				[0.5, 0.0,  0.5],	
+				[0.5, 0.0, -0.5]		
+			]
+        ];
+        
+        this.nurbs_surface = new CGFnurbsSurface(1, 1, controlPoints);
+        this.plane = new CGFnurbsObject(this.scene, npartsU, npartsV, nurbs_surface);  
+    }
+    
+    display() {
+		this.plane.display();
+	}
+}
