8433f0b49016c2ab7123c5486c501327e6ad52d2
Ana Teresa Cruz <up201806460@fe.up.pt>
2020-11-18 11:23:54 +0000

    Added defbarrel primitive

---
+++ TP2/primitives/MyDefbarrel.js
@@ -0,0 +1,61 @@
+/**
+ * MyDefbarrel
+ * @constructor
+ * @param {CGFscene} scene - Reference to MyScene object
+ * @param {float} base
+ * @param {float} middle
+ * @param {float} height
+ * @param {integer} slices
+ * @param {integer} stacks
+ */
+class MyDefbarrel extends CGFobject {
+	constructor(scene, base, middle, height, slices, stacks) {
+        super(scene);
+        this.base = base;
+        this.middle = middle;
+        this.height = height;
+        this.slices = slices;
+        this.stacks = stacks;
+
+        let angle = Math.PI / 6;
+        let h = 4 / 3 * this.base;
+        let H = 4 / 3 * (This.middle - this.base);
+        let aux = this.height / Math.tan(Math.PI / 3);
+        
+        let controlPoints = [
+        [
+            [-this.base, 0, this.height, 1],
+            [-this.base - H, 0, this.height - aux, 1],
+            [-this.base - H, 0, aux, 1],
+            [-this.base, 0, 0, 1],
+        ],
+
+        [
+            [-this.base, h, this.height, 1],
+            [(this.base + H) * Math.cos(Math.PI - angle), Math.sin(Math.PI - angle) * (this.base + h), this.height - aux, 1],
+            [(this.base + H) * Math.cos(Math.PI - angle), Math.sin(Math.PI - angle) * (this.base + h), aux, 1],
+            [-this.base, h, 0, 1],
+        ],
+
+        [
+            [this.base, h, this.height, 1],
+            [(this.base + H) * Math.cos(angle), Math.sin(angle) * (this.base + h), this.height - aux, 1],
+            [(this.base + H) * Math.cos(angle), Math.sin(angle) * (this.base + h), aux, 1],
+            [this.base, h, 0, 1],
+        ],
+
+        [
+            [this.base, 0, this.height, 1],
+            [this.base + H, 0, this.height - aux, 1],
+            [this.base + H, 0, aux, 1],
+            [this.base, 0, 0, 1],
+        ],
+       ];
+
+        this.defbarrel = new MyPatch(this.scene, this.stacks, this.slices, 3, 3, controlPoints);
+    }
+    
+    display() {
+		this.defbarrel.display();
+	}
+}
