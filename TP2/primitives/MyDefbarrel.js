bf8e6f4f348b7a39e6d43b69b17fdf6ffd01c56e
Andre Nascimento <up201806461@fe.up.pt>
2020-11-19 09:54:27 +0000

    fixed barrel

--- TP2/primitives/MyDefbarrel.js
+++ TP2/primitives/MyDefbarrel.js
@@ -20,37 +20,41 @@ class MyDefbarrel extends CGFobject {
         let angle = Math.PI / 6;
         let h = 4 / 3 * this.base;
         let H = 4 / 3 * (this.middle - this.base);
-        let aux = this.height / Math.tan(Math.PI / 3);
+        
+        let middleX = (this.base + H);
+        let middleY = (4/3) * middleX;
+        let Q2Z = H / Math.tan(angle);
+        let Q3Z = this.height - Q2Z;
         
         let controlPoints = [
+            //P4
+            [
+                [this.base, 0, 0, 1],           //Q1
+                [middleX, 0, Q2Z, 1],           //Q2
+                [middleX, 0, Q3Z, 1],           //Q3
+                [this.base, 0, this.height, 1]  //Q4
+            ],
+            //P3
+            [
+                [this.base, h, 0, 1],           //Q1
+                [middleX, middleY, Q2Z, 1],     //Q2
+                [middleX, middleY, Q3Z, 1],     //Q3
+                [this.base, h, this.height, 1]  //Q4
+            ],
+            //P2
+            [
+                [-this.base, h, 0, 1],          //Q1
+                [-middleX, middleY, Q2Z, 1],    //Q2
+                [-middleX, middleY, Q3Z, 1],    //Q3
+                [-this.base, h, this.height, 1] //Q4
+            ],
             //P1
             [
-                [-this.base, 0, this.height, 1], //Q4
-                [-this.base - H, 0, aux, 1], //Q3
-                [-this.base - H, 0, this.height - aux, 1], //Q2
-                [-this.base, 0, 0, 1], //Q1
-            ],
-
-            [
-                [-this.base, h, this.height, 1],
-                [(this.base + H) * Math.cos(Math.PI - angle), Math.sin(Math.PI - angle) * (this.base + h), aux, 1],
-                [(this.base + H) * Math.cos(Math.PI - angle), Math.sin(Math.PI - angle) * (this.base + h), this.height - aux, 1],
-                [-this.base, h, 0, 1],
-            ],
-
-            [
-                [this.base, h, this.height, 1],
-                [(this.base + H) * Math.cos(angle), Math.sin(angle) * (this.base + h), aux, 1],
-                [(this.base + H) * Math.cos(angle), Math.sin(angle) * (this.base + h), this.height - aux, 1],
-                [this.base, h, 0, 1],
-            ],
-
-            [
-                [this.base, 0, this.height, 1],
-                [this.base + H, 0, aux, 1],
-                [this.base + H, 0, this.height - aux, 1],
-                [this.base, 0, 0, 1],
-            ],
+                [-this.base, 0, 0, 1],          //Q1
+                [-middleX, 0, Q2Z, 1],          //Q2
+                [-middleX, 0, Q3Z, 1],          //Q3
+                [-this.base, 0, this.height, 1] //Q4
+            ]
         ];
 
         console.log(controlPoints);
