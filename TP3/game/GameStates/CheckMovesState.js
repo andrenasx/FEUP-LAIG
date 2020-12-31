6e65d4db688e789a9c8260c7121ae1cc6ac0c894
Andre Nascimento <up201806461@fe.up.pt>
2020-12-31 10:10:25 +0000

    GameStates comments

--- TP3/game/GameStates/CheckMovesState.js
+++ TP3/game/GameStates/CheckMovesState.js
@@ -8,10 +8,12 @@ class CheckMovesState extends GameState {
         console.log(message);
         if(message>0){
             console.log("Has moves");
+            // If there are available moves, select a piece to move
             this.gameOrchestrator.changeState(new SelectState(this.gameOrchestrator));
         }
         else {
             console.log("No moves");
+            // Else select a piece to remove
             this.gameOrchestrator.changeState(new RemoveState(this.gameOrchestrator));
         }
         
--- TP3/game/GameStates/InitialState.js
+++ TP3/game/GameStates/InitialState.js
@@ -5,6 +5,7 @@ class InitialState extends GameState {
 
     pickObj(id) {
         if(id==101){
+            // Start game when play button is pressed
             this.gameOrchestrator.play();
             document.getElementById("time").innerHTML = "";
         }
