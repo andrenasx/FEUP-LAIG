415e314e215eeefb2d582bc4de414ac190a23346
Ana Teresa Cruz <up201806460@fe.up.pt>
2020-12-31 14:56:48 +0000

    Material are defined or changed here

--- TP3/game/MyButton.js
+++ TP3/game/MyButton.js
@@ -4,11 +4,31 @@ class MyButton extends CGFobject {
         this.text = text;
         this.cube = new MyCube(this.scene);
         this.buttonText = new MySpriteText(this.scene, this.text);
+
+        //Materials
+
+        this.lightgreyMaterial = new CGFappearance(this.scene);
+        this.lightgreyMaterial.setShininess(5);
+        this.lightgreyMaterial.setEmission(0, 0, 0, 1);
+        this.lightgreyMaterial.setAmbient(0.76, 0.77, 0.8, 1);
+        this.lightgreyMaterial.setDiffuse(0.76, 0.77, 0.8, 1),
+        this.lightgreyMaterial.setSpecular(0, 0, 0, 1);
+
+        this.greenMaterial = new CGFappearance(this.scene);
+        this.greenMaterial.setShininess(5);
+        this.greenMaterial.setEmission(0, 0, 0, 1);
+        this.greenMaterial.setAmbient(0.04, 0.58, 0.25, 1);
+        this.greenMaterial.setDiffuse(0.04, 0.58, 0.25, 1),
+        this.greenMaterial.setSpecular(0, 0, 0, 1);
+
+        this.selectedMaterial = this.lightgreyMaterial;
+        this.available = false;
     }
 
     display() {
         this.scene.pushMatrix();
         this.scene.scale(0.45, 0.1, 0.2);
+        this.selectedMaterial.apply();
         this.cube.display();
         this.scene.popMatrix();
 
@@ -17,5 +37,15 @@ class MyButton extends CGFobject {
         this.scene.scale(0.08, 0.06, 1);
         this.buttonText.display();
         this.scene.popMatrix();
+    }
+
+    toggleAvailability() {
+        this.available = !this.available;
+        if(this.available) {
+            this.selectedMaterial = this.greenMaterial;
+        }
+        else {
+            this.selectedMaterial = this.lightgreyMaterial;
+        }
     }
 }
